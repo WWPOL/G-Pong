8244bc795552d8821a0d071a80467d70c0b38dfa
philsiff <filman74@verizon.net>
2015-05-23 12:15:52 -0400

    Removed Merge Error

--- public/js/clientInfo.js
+++ public/js/clientInfo.js
@@ -7,7 +7,6 @@ ClientInfo.prototype.getWell = function() {
 	return this.well;
 };
 
-<<<<<<< HEAD
 ClientInfo.prototype.getClientNum = function(){
 	return this.clientNum;
 }
@@ -23,8 +22,7 @@ ClientInfo.prototype.setWellY = function(y){
 ClientInfo.prototype.setClientNum = function(num){
 	this.clientNum = num;
 }
-=======
 ClientInfo.prototype.setWell = function(well) {
 	this.well = well;
 };
->>>>>>> 767a885b7044b65dac647cd6e15090c2eef501c7
+
