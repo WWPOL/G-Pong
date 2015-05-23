145851f9d26749a43a55a5d8db4234fbb2ff50a2
philsiff <filman74@verizon.net>
2015-05-23 13:02:19 -0400

    Changed Paddle

--- public/js/paddle.js
+++ public/js/paddle.js
@@ -6,6 +6,9 @@ Paddle = function(playerIndex) {
 
 	this.image = new Image();
 
+	this.height = 200;
+	this.width = 20;
+
 	if (playerIndex === 0) {
 		this.image.src = "../assets/paddle-blue.png";
 
@@ -14,14 +17,14 @@ Paddle = function(playerIndex) {
 	else if (playerIndex === 1) {
 		this.image.src = "../assets/paddle-red.png";
 
-		this.x = gameCanvas.width - this.image.width - X_OFFSET;
+		this.x = gameCanvas.width - this.width - X_OFFSET;
 	}
 
-	this.y = DEFAULT_Y - this.image.height / 2;
+	this.y = DEFAULT_Y - this.height / 2;
 }
 
 Paddle.prototype.render = function() {
-gameContext.drawImage(this.image, this.getX(), this.getY());
+	gameContext.drawImage(this.image, this.getX(), this.getY(), this.getWidth(), this.getHeight());
 }
 
 Paddle.prototype.getX = function() {
@@ -35,3 +38,11 @@ Paddle.prototype.getY = function() {
 Paddle.prototype.setY = function(y) {
 	this.y = y;
 }
+
+Paddle.prototype.getHeight = function() {
+	return this.height;
+};
+
+Paddle.prototype.getWidth = function(){
+	return this.width;
+};
