e5a4cbc3c1eeef90c29337a45f64d4d8781524f3
Andrew <liboan1997@gmail.com>
2015-05-23 10:58:14 -0400

    server 'starts game' and disconnects extra users

--- index.js
+++ index.js
@@ -12,10 +12,32 @@ app.get('/', function(req, res){
 var users = [];
 
 io.on("connection", function (socket) {
-	console.log("A user joined");
+	console.log("User " + socket.id + " joined");
 	users.push(socket.id);
 	console.log(users.length + " users connected");
+
+	socket.on("disconnect", function () {
+		console.log("User " + socket.id + " left");
+		users.splice(users.indexOf(socket.id));
+		console.log(users.length + " users connected");
+	});
+
+	if (users.length == 2) { //when 2 players connect, start game
+		console.log("Starting game with users " + users);
+		io.emit("start");
+	}
+
+	if (users.length > 2) { //if there's already two players, kick anyone else
+		console.log("Server is full, goodbye " + socket.id);
+		io.to(socket.id).emit("full");
+		socket.disconnect();
+	}
 });
+
+function sendToAll() {
+
+}
+
 
 http.listen(7777, function(){
 	console.log("listening on port 7777");
