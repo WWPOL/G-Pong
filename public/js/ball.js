02ef6732733e643b467f8c58f8807e6e5ad6e2cc
philsiff <filman74@verizon.net>
2015-05-23 11:45:58 -0400

    Added radius to ball

--- public/js/ball.js
+++ public/js/ball.js
@@ -1,9 +1,10 @@
-Ball = function(x, y, mass, movement) {
+Ball = function(x, y, radius, mass, movement) {
 	this.image = new Image();
 	image.src = "../assets/ball-blue.png"
 
 	this.x = x;
 	this.y = y;
+	this.radius = radius;
 	this.mass = mass;
 	this.movement = movement;
 };
@@ -24,11 +25,15 @@ Ball.prototype.getMovement = function(){
 	return this.movement;
 };
 
+Ball.prototype.getRadius = function() {
+	return this.radius;
+};
+
 Ball.prototype.update = function(delta) {
 	this.x += this.movement.getX();
 	this.y += this.movement.getY();
 };
 
 Ball.prototype.render = function(){
-	gameContext.drawImage(image, this.getX(), this.getY());
+	gameContext.drawImage(image, this.getX(), this.getY(), this.radius * 2, this.radius * 2 );
 };
